3ac819bde0bbd953acb4ad374fa9ece2f5c37f79
Marvin Parke <mp1pro@gmail.com>
2017-01-04 18:23:05 -0500

    Update deposit.php

--- deposit.php
+++ deposit.php
@@ -2,12 +2,9 @@
     // configuration
     require("includes/config.php");
     
-    if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        if(!empty($_POST["deposit"]))
-        {
-            if(is_numeric($_POST["deposit"]) || preg_match("/^\d+$/", $_POST["deposit"]))
-            {
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(!empty($_POST["deposit"])){
+            if(is_numeric($_POST["deposit"]) || preg_match("/^\d+$/", $_POST["deposit"])){
                 $query = query("UPDATE users SET cash = cash + ? where id = ?", $_POST["deposit"], $_SESSION["id"]);
                 $action="deposit";
                 $symbol="$";
